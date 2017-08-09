547eec8c2fc459778f023bcc08cc13a83702fc1e
Darth Skywalker <adi2point0@gmail.com>
2017-08-09 21:02:42 +0530

    minor changes

--- src/components/app/app.component.js
+++ src/components/app/app.component.js
@@ -2,55 +2,43 @@ import CodeMirror from 'codemirror';
 import MirrorConsole from 'codemirror-console';
 
 class AppController {
-    constructor($element) {
+    constructor($http, $element) {
+        this.$http = $http;
         this.$element = $element;
     }
 
     $onInit() {
+        this.$outputEl = this.$element.find('#output'); 
         this.outputEl = this.$element.find('#output')[0];
         this.editorEl = this.$element.find('#editor')[0];
         this.editorConfig = {
+            lint: { esversion: 6 },
             lineNumbers: true,
-            mode: {
-                name: 'javascript',
-                json: true
-            },
-            theme: 'blackboard'
+            mode: 'javascript',
+            theme: 'blackboard',
+            runnable: true,
+            matchBrackets: true,
+            autoCloseBrackets: true,
+            scrollbarStyle: 'null',
+            lineWrapping: true,
+            gutters: ['CodeMirror-lint-markers']
         }
         this.editor = CodeMirror.fromTextArea(this.editorEl, this.editorConfig);
     }
 
     run() {
-        const code = this.editor.getValue();
-
-        const scriptTag = angular.element('<script/>', {text:code});
-
-        this.$element.append(scriptTag);
-
-        /*const tempEditor = new MirrorConsole();
-        tempEditor.setText(this.editorEl.textContent);
-        tempEditor.swapWithElement(this.editorEl);
-        var consoleMock = {
-            log: function (arg) {
-                function line(text) {
-                    var div = document.createElement("div");
-                    div.appendChild(document.createTextNode(text));
-                    return div;
-                }
-                this.outputEl.appendChild(line(arg));
-            }
-        }
-        // eval code
-        tempEditor.runInContext({ console: consoleMock }, function (error, result) {
-            if (error) {
-                console.error(error);
-            }
-        });
-        tempEditor.destroy();// remote editor*/
+        this.$http.post('http://localhost:8090/code',{code:this.editor.getValue()})
+            .then(response => {
+                this.$outputEl.html(response.data.result);
+            });
+        /*const code = this.editor.getValue();
+        const scriptTag = angular.element('<script/>', { text: code });
+        this.$element.append(scriptTag);*/
     }
 }
 
 AppController.$onInit = [
+    '$http',
     '$element'
 ];
 
